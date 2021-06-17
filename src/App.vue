d208daf8842914433bbb42688ef4c1ba0402280a
Javier Alarcon <javier.alarcon@pragma.com.co>
2021-06-17 11:12:43 -0500

    feat: added header and footer to app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <app-header></app-header>
     <router-view/>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
 export default {
+  components: { AppHeader, AppFooter },
   name: 'App'
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  color: #37474f;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  img{
-    width: 20%;
-  }
+  margin: 0 auto;
+  padding: 0;
+}
+.container {
+  padding-bottom: 4rem;
 }
 </style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <footer>
+    <span>Hecho con amor y <a href="//vuejs.org">vue.js</a></span>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer'
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  box-sizing: border-box;
+  background-color: #cfd8dc;
+  bottom: 0;
+  left: 0;
+  padding: 1rem;
+  position: absolute;
+  width: 100%;
+  a {
+    text-decoration: none;
+    color: #546e74;
+  }
+}
+</style>
