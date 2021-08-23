d036e9018a2664180df7c4bb87316e932a0726fa
Javier Alarcon <javier.alarcon@pragma.com.co>
2021-08-23 08:48:06 -0500

    add setting.js

---
+++ src/api/settings.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyD8yRYzYPttT8B6LEX_WP2nFWfBEO4WNGI',
+  authDomain: 'my-todo-39f08.firebaseapp.com',
+  projectId: 'my-todo-39f08',
+  storageBucket: 'my-todo-39f08.appspot.com',
+  messagingSenderId: '83614745067',
+  appId: '1:83614745067:web:daf5df5668b93a806d19ae',
+  measurementId: 'G-5SJ65WELQZ'
+};
